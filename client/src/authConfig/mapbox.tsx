ecd5366791de0f54aa349cf8fa3fa7b2f77e28f0
Mikky <meekyberry6@gmail.com>
2023-07-08 03:35:31 +0100

    feat: Removed cached files

--- client/src/authConfig/mapbox.tsx
+++
@@ -1,5 +0,0 @@
-import mapboxgl from "mapbox-gl";
-
-mapboxgl.accessToken = "pk.eyJ1IjoibWlra3lsYW5reSIsImEiOiJjbGpkY3JsdnMydDg4M2Zxbnp6ZTAzZGtmIn0.kcv2OWbrTXW0nGbIJGTtmQ"
-
-export const accessToken = mapboxgl.accessToken;
