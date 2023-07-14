a99af46ffbe82a9d93eb868615db412ef9b2d6e2
Mikky <meekyberry6@gmail.com>
2023-07-14 20:19:50 +0100

    feat: removed cached files

--- server/config/allowedOrigins.js
+++
@@ -1,8 +0,0 @@
-const allowedOrigins = [
-  'http://localhost:5000',
-  'http://localhost:5173',
-  "https://findhospitals.netlify.app",
-  "https://www.findhospitals.netlify.app"
-]
-
-export default allowedOrigins;
